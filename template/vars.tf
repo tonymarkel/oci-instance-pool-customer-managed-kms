e4b6944992cebe4d32b8bd6f556df942e0b73198
tonymarkel <32904980+tonymarkel@users.noreply.github.com>
2023-02-24 10:31:51 -0500

    Add files via upload

---
+++ template/vars.tf
@@ -0,0 +1,7 @@
+variable compartment_ocid { default = "ocid1.compartment.oc1..aaaaaaaazf5hoo75ysa373xpq2pwvors7vx6fdvnnqg7qbgt75gjjnuxu3oq" }
+variable baseline_image { default = "ocid1.image.oc1.iad.aaaaaaaa4m3bxzwpfcrthy3x74rozkmsxpnru4zcmlmta3yehg3rptvui7jq" }
+variable kms_key_ocid { default = "ocid1.key.oc1.iad.b5r64lp5aad6q.abuwcljr5happi4jaomyvg52rcjzvatotg65yzkvy3sjiom3usgbxpq67bdq"}
+variable availability_domain { default = "rgiR:US-ASHBURN-AD-1" }
+variable fault_domain { default = "FAULT-DOMAIN-3" }
+variable region { default = "us-ashburn-1" }
+variable subnet { default = "ocid1.subnet.oc1.iad.aaaaaaaah53gukgprdviouwym7o5hprih3mfnwb2k2u4b56p7ikdk3l33nqa"}
